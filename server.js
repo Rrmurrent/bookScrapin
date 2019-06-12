d0827071e713d9abc86e62cabebbe6704f51075d
Smarty Pants <amcurrent@hotmail.com>
2019-06-12 14:22:03 -0500

    trying to figure things out

--- server.js
+++ server.js
@@ -1,24 +1,58 @@
-const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+// const $ = require('jquery');
+const express = require('express');
+const Router = express.Router()
+const path = require('path');
+const mongojs = require('mongojs');
+const mongoose = require('mongoose');
+var logger = require('morgan');
+const axios = require('axios');
+
 const app = express();
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3001;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/SavedBooks";
+const GOOGLY_API = process.env.GOOGLY_API;
+
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.use(logger("dev"))
+
+mongoose.connect(MONGODB_URI);
+
+const dbRoute = require(path.join(__dirname, "./models/book.js"))
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
 }
 
-// Define API routes here
+app.get('/api/books', (req, res) => {
+    dbRoute.find({}, function(err, result) {
+        if(err){
+            throw err;
+        } else {
+            res.json(result)
+        }
+    })
+})
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.post('/api/books', (req, res) => {
+    dbRoute.create(req.body).then(data =>{
+        res.send("Book Saved!")
+    }).catch(err => {throw err;})
+})
+
+app.delete('/api/books/:id', (req, res) => {
+    const bookId = req.params.id
+    dbRoute.deleteOne({"id": bookId}, function(error, data){
+        if (error) throw error;
+        res.send('Book Deleted!')
+    })
+})
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+    console.log(`🌎 ==> API server now on port ${PORT}!`)
+})
--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Header from './components/Header';
+import SearchBar from "./components/SearchBar";
+import SavedBooks from "./components/SavedBooks"
+// import Results from "./assets/Results"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    data: null
+  };
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Header />
+          <Route exact = {true} path = "/" component={SearchBar}>
+            {/* <SearchBar /> */}
+          </Route>
+          <Route exact = {true} path = "/saved" component = {SavedBooks}>
+            {/* <SavedBooks /> */}
+          </Route>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
